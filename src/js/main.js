224b8f574e815c7b96d23d3bd562866d740cb8a2
thiamsantos <thiamsantos@gmail.com>
2017-02-12 15:24:14 -0200

    add swipe gestures to menu offcanvas

--- src/js/main.js
+++ src/js/main.js
@@ -12,13 +12,30 @@ const menu = document.querySelector('.js-menu-offcanvas')
 const button = document.querySelector('.js-button-offcanvas')
 const overlay = document.querySelector('.js-overlay-offcanvas')
 
-button.addEventListener('click', e => {
-  console.log(e)
+button.addEventListener('click', () => {
   menu.classList.add('is-open')
   overlay.classList.add('is-open')
 })
-overlay.addEventListener('click', e => {
-  console.log(e)
+overlay.addEventListener('click', () => {
   menu.classList.remove('is-open')
   overlay.classList.remove('is-open')
+})
+
+let touchstart
+
+document.addEventListener('touchstart', e => {
+  touchstart = e.touches[0].clientX
+})
+
+document.addEventListener('touchend', e => {
+  const touchend = e.changedTouches[0].clientX
+
+  if (touchstart < 20 && touchend > touchstart) {
+    button.click()
+    return false
+  }
+
+  if (touchend < touchstart && menu.classList.contains('is-open')) {
+    overlay.click()
+  }
 })
