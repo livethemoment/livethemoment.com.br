2781744bd7037fa4fec47052bd09426671bb1d38
Thiago Santos <thia.mdossantos@gmail.com>
2017-03-30 11:09:34 -0300

    Configure gulp and make some changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,104 +1,101 @@
-var gulp = require('gulp'),
-    browserSync = require('browser-sync').create(),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
-    postcss = require('gulp-postcss'),
-    imageResize = require('gulp-image-resize'),
-    parallel = require("concurrent-transform"),
-    os = require("os"),
-    cp = require('child_process');
+const cp = require('child_process')
+const os = require('os')
+const autoprefixer = require('gulp-autoprefixer')
+const browserSync = require('browser-sync').create()
+const cssnano = require('gulp-cssnano')
+const gulp = require('gulp')
+const jimp = require('gulp-jimp')
+const parallel = require('concurrent-transform')
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')
 
-var messages = {
+const messages = {
   jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
-};
+}
 
 /**
  * Build the Jekyll Site
  */
-gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn('jekyll', ['build', '--config=_config.yml'], {stdio: 'inherit'})
-        .on('close', done);
-});
+gulp.task('jekyll-build', done => {
+  browserSync.notify(messages.jekyllBuild)
+  return cp.spawn('jekyll.bat', ['build', '--config=_config.yml'], {stdio: 'inherit'})
+        .on('close', done)
+})
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', ['styles', 'jekyll-build'], function() {
+gulp.task('browser-sync', ['styles', 'jekyll-build'], () => {
   browserSync.init({
     server: {
       baseDir: '_site'
     },
-    startPath: "/index.html"
-  });
-});
-
-// To support opacity in IE 8
-
-var opacity = function(css) {
-  css.eachDecl(function(decl, i) {
-    if (decl.prop === 'opacity') {
-      decl.parent.insertAfter(i, {
-        prop: '-ms-filter',
-        value: '"progid:DXImageTransform.Microsoft.Alpha(Opacity=' + (parseFloat(decl.value) * 100) + ')"'
-      });
-    }
-  });
-};
+    startPath: '/index.html'
+  })
+})
 
 /**
  * Compile files from sass into both assets/css (for live injecting) and site (for future jekyll builds)
  */
-gulp.task('styles', function() {
+gulp.task('styles', () => {
   return gulp.src('_scss/main.scss')
-    .pipe(sass({ outputStyle: 'expanded' }))
-    .pipe(autoprefixer({browsers: ['last 2 versions', 'Firefox ESR', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1']}))
-    .pipe(postcss([opacity]))
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(autoprefixer({
+      browsers: [
+        'last 2 versions',
+        'Firefox ESR',
+        'safari 5',
+        'ie 9',
+        'opera 12.1'
+      ]
+    }))
     .pipe(gulp.dest('assets/css'))
     .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('assets/css'));
-});
+    .pipe(cssnano())
+    .pipe(gulp.dest('assets/css'))
+})
 
 /**
  * Automatically resize post feature images and turn them into thumbnails
  */
-gulp.task("thumbnails", function () {
-  gulp.src("assets/images/hero/*.{jpg,png}")
+gulp.task('thumbnails', () => {
+  gulp.src('assets/images/hero/*.{jpg,png}')
     .pipe(parallel(
-      imageResize({ width : 350 }),
+      jimp({
+        '': {
+          resize: {width: 350}
+        }
+      }),
       os.cpus().length
     ))
-    .pipe(gulp.dest("assets/images/thumbnail"));
-});
+    .pipe(gulp.dest('assets/images/thumbnail'))
+})
 
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll
  * Watch _site generation, reload BrowserSync
  */
-gulp.task('watch', function() {
-  gulp.watch('_scss/**/*.scss', ['styles']);
-  gulp.watch('assets/images/hero/*.{jpg,png}', ['thumbnails']);
-  gulp.watch(['*.html',
-          '*.txt',
-          'about/**',
-          '_posts/*.markdown',
-          'assets/javascripts/**/**.js',
-          'assets/images/**',
-          'assets/fonts/**',
-          '_layouts/**',
-          '_includes/**',
-          'assets/css/**'
-        ],
-        ['jekyll-build']);
-  gulp.watch("_site/index.html").on('change', browserSync.reload);
-});
+gulp.task('watch', () => {
+  gulp.watch('_scss/**/*.scss', ['styles'])
+  gulp.watch('assets/images/hero/*.{jpg,png}', ['thumbnails'])
+  gulp.watch([
+    '*.html',
+    '*.txt',
+    'about/**',
+    '_posts/*.md',
+    'assets/javascripts/**/**.js',
+    'assets/images/**',
+    'assets/fonts/**',
+    '_layouts/**',
+    '_includes/**',
+    'assets/css/**'
+  ], ['jekyll-build'])
+  gulp.watch('_site/index.html').on('change', browserSync.reload)
+})
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['thumbnails', 'browser-sync', 'watch']);
+gulp.task('default', ['thumbnails', 'browser-sync', 'watch'])
